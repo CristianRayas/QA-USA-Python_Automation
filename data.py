83449e059807bdde0df8d192e782749cb81d4ac7
cristianrayas <lights303@gmail.com>
2026-04-20 07:56:10 -0600

    Initial commit

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-URBAN_ROUTES_URL = ''
+URBAN_ROUTES_URL = 'https://cnt-50594baa-8c03-4582-89a3-64c714cbbb31.containerhub.tripleten-services.com'
 
 ADDRESS_FROM = 'East 2nd Street, 601'
 ADDRESS_TO = '1300 1st St'
